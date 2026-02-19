eb81f750e814397a2ede28664c9e1df44ca36071
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-19 10:50:33 +0200

    add add insertion time func

--- consumer/main.py
+++ consumer/main.py
@@ -1,4 +1,5 @@
 from redis_connection import get_connection
+from datetime import datetime
 import json
 
 
@@ -10,3 +11,7 @@ def load_from_redis(queue):
     metadata_info = json.loads(metadata[1].decode('utf-8'))
     full_message = r.hgetall(f"message:{metadata_info['id']}")
     return full_message
+
+def add_insertion_time(alert):
+    alert['time_insertion'] = datetime.now()
+    return alert
