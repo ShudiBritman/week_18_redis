b7e9e4348b56695f9fc81a039ff592f3f37eed82
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-19 09:50:54 +0200

    add check_combine_condition func

--- producer/priority_logic.py
+++ producer/priority_logic.py
@@ -11,3 +11,14 @@ def classifies_events(event):
         return True
     elif vehicle_type == "truck":
         return True
+    
+
+def check_combine_condition(event):
+    distance_from_fence_m = event['distance_from_fence_m']
+    people_count = event['people_count']
+    vehicle_type = event['vehicle_type']
+    if people_count >= 4 and distance_from_fence_m <= 150:
+        return True
+    elif people_count >= 3 and vehicle_type == "jeep":
+        return True
+    
