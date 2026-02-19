f203b8eaf33b18401880819c5b39ef7fe95eec4b
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-19 12:30:24 +0200

    add border_distribution query

--- analytics-api/dal.py
+++ analytics-api/dal.py
@@ -8,25 +8,64 @@ def get_collection():
 class Queri:
     @staticmethod
     def border_distribution():
-        pass
-    
+        coll = get_collection()
+        query = {'border': 'URGENT'}
+        projection = {'border':1, '_id':0}
+        cursor = list(coll.find(query, projection))
+        pipline = [
+             {
+                  '$group':{
+                       '_id':{
+                            'priority': '$priority',
+                            'count': {'count': 'count'}
+                       }
+                  }
+             },
+             {'$project':
+              {'_id':0,
+               'priority':'$_id.priority',
+               'count':'_id.count'}
+              },
+              {'$sort':{'count':-1}}
+        ]
+        result = list(coll.aggregate(pipline))
+        return {cursor, result}
+
 
     @staticmethod
     def top_5_area():
-        pass
-
+        coll = get_collection()
+        pipline = [
+             {
+                  '$group':{
+                       '_id':{
+                            'priority': '$priority',
+                            'count': {'count': 'count'}
+                       }
+                  }
+             },
+             {'$project':
+              {'_id':0,
+               'priority':'$_id.priority',
+               'count':'_id.count'}
+              },
+              {'$sort':{'count':-1}},
+              {'$limit': 5}
+        ]
+        return list(coll.aggregate(pipline))
+        
 
     @staticmethod
     def distance_distribution():
-        pass
+        coll = get_collection()
     
 
     @staticmethod
     def dangerous_area():
-        pass
+        coll = get_collection()
 
 
     @staticmethod
     def close_and_urgent_alerts():
-        pass
+        coll = get_collection()
 
