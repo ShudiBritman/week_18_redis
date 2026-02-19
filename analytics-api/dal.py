90d6d0c8a477115ccb51aeee1dd5b1ea1e0d581c
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-19 11:57:55 +0200

    add dal funcs

--- analytics-api/dal.py
+++ analytics-api/dal.py
@@ -5,3 +5,28 @@ def get_collection():
     return MongoConnection().get_collection()
 
 
+class Queri:
+    @staticmethod
+    def border_distribution():
+        pass
+    
+
+    @staticmethod
+    def top_5_area():
+        pass
+
+
+    @staticmethod
+    def distance_distribution():
+        pass
+    
+
+    @staticmethod
+    def dangerous_area():
+        pass
+
+
+    @staticmethod
+    def close_and_urgent_alerts():
+        pass
+
